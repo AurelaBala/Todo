38ef8fcf84376f7df3a0c659bd5f7ffa1372acd7
AurelaBala <aurelatosuni2022@gmail.com>
2022-11-10 19:29:17 -0500

    creating a prototype cell and populate the table view dynamically

--- Todo/MarkAsDoneTableViewCell.swift
+++ Todo/MarkAsDoneTableViewCell.swift
@@ -3,7 +3,7 @@
 //  Todo
 //
 //  Created by Aurela Bala on 2022-11-10.
-//
+// Custom cell
 
 import UIKit
 
@@ -15,5 +15,34 @@ class MarkAsDoneTableViewCell: UITableViewCell {
     @IBOutlet weak var taskDueDate: UILabel!
     
     @IBOutlet weak var taskSwitch: UISwitch!
+    
+    
+    @IBAction func completed(_ sender: UISwitch)
+    {
+        updateCompleted()
+    }
+    //method for title, due date and switcher to not be accessed directly
+    
+    func set(title: String, due_date: String, isCompleted: Bool)
+    {
+        taskLabel.text = title
+        taskDueDate.text = due_date
+        taskSwitch.isOn = isCompleted
+        updateCompleted()
+    }
+    
+    //check if the switcher is on or off and then add/remove attribute
+    private func updateCompleted ()
+    {
+        let attributedString = NSMutableAttributedString(string: taskLabel.text!)
+        
+        if taskSwitch.isOn {
+            attributedString.addAttribute(.strikethroughStyle, value: 2, range: NSMakeRange(0, attributedString.length-1))
+        }
+        else {
+            attributedString.removeAttribute(.strikethroughStyle, range: NSMakeRange(0, attributedString.length-1))
+        }
+        taskLabel.attributedText = attributedString
+    }
     
 }
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -18,8 +18,15 @@ class ViewController: UIViewController {
     ]
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addTaskButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTaskButton.layer.shadowColor = UIColor.white.cgColor
+        addTaskButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+            addTaskButton.layer.shadowRadius = 5
+            addTaskButton.layer.shadowOpacity = 1.0
+        
+        self.tableView.layer.cornerRadius = 10.0
         // Do any additional setup after loading the view.
     }
 
@@ -30,8 +37,7 @@ extension ViewController: UITableViewDataSource
 {
 
 //    func numberOfSections(in tableView: UITableView) -> Int {
-//        return todotasks.count
-//    }
+//        return 2    }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return todotasks.count
@@ -39,13 +45,16 @@ extension ViewController: UITableViewDataSource
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
+        //method for tableView to create the cell table for us
+        let cell = tableView.dequeueReusableCell(withIdentifier: "checked cell", for: indexPath) as! MarkAsDoneTableViewCell
+        
         let task = todotasks[indexPath.row]
-        let cell = UITableViewCell()
+        //let cell = MarkAsDoneTableViewCell()
         let title = task.title
         let due_date = task.due_date
-        cell.textLabel!.numberOfLines = 0
-        cell.textLabel?.text = title + "\n" + due_date
-       
+        //cell.textLabel!.numberOfLines = 0
+        //cell.textLabel?.text = title + "\n" + due_date
+        cell.set(title: title, due_date: due_date, isCompleted: task.isCompleted)
         return cell
     }
   
