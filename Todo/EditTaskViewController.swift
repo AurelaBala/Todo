7e90f026e1094ac5767d09a004c69da085aaf85a
AurelaBala <aurelatosuni2022@gmail.com>
2022-11-12 19:27:04 -0500

    Edit task Landscape created

--- Todo/EditTaskViewController.swift
+++ Todo/EditTaskViewController.swift
@@ -16,12 +16,17 @@ class EditTaskViewController: UIViewController {
     @IBOutlet weak var editTitle: UITextField!
     @IBOutlet weak var taskDescription: UITextView!
     
+    @IBOutlet weak var editTaskView: UIView!
+    @IBOutlet weak var editTitleLandscape: UITextField!
+    @IBOutlet weak var taskDescriptionLandscape: UITextView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.editView.layer.cornerRadius = 10.0
+        editTaskView.layer.cornerRadius = 10.0
         editTitle.layer.cornerRadius = 10
         taskDescription.layer.cornerRadius = 10
         editTitle.layer.borderWidth = 1
@@ -29,6 +34,12 @@ class EditTaskViewController: UIViewController {
         editTitle.layer.borderColor = UIColor(hue: 0.65, saturation: 0.57, brightness: 0.92, alpha: 1).cgColor
         taskDescription.layer.borderColor = UIColor(hue: 0.65, saturation: 0.57, brightness: 0.92, alpha: 1).cgColor
         
+        editTitleLandscape.layer.cornerRadius = 10
+        taskDescriptionLandscape.layer.cornerRadius = 10
+        editTitleLandscape.layer.borderWidth = 1
+        taskDescriptionLandscape.layer.borderWidth = 1
+        editTitleLandscape.layer.borderColor = UIColor(hue: 0.65, saturation: 0.57, brightness: 0.92, alpha: 1).cgColor
+        taskDescriptionLandscape.layer.borderColor = UIColor(hue: 0.65, saturation: 0.57, brightness: 0.92, alpha: 1).cgColor
     }
     
     
